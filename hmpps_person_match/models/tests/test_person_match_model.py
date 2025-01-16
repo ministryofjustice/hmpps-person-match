7b291dd5bca28986fe7bef4d89014d9fb69ef726
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-01-16 14:20:29 +0000

    CPR-552 Remove old models

--- hmpps_person_match/models/tests/test_person_match_model.py
+++ hmpps_person_match/models/tests/test_person_match_model.py
@@ -1,7 +1,3 @@
-import pytest
-from pydantic import ValidationError
-
-from hmpps_person_match.models.person_match_model import MatchingFromPerson, MatchingToPerson, PersonMatching
 
 
 class TestPersonMatchModel:
@@ -9,49 +5,8 @@ class TestPersonMatchModel:
     Test person matching model
     """
 
-    def test_model_populates_source_dataset(self):
+    def test_person_model(self):
         """
         Test model populates the source dataset property accordingly
         """
-        person_match_model = PersonMatching(
-            matching_from=MatchingFromPerson(unique_id="1", pnc="1234567890"),
-            matching_to=[MatchingToPerson(unique_id="2", pnc="1234567890")],
-        )
-        assert person_match_model.matching_from.source_dataset == "matching_from"
-        assert person_match_model.matching_to[0].source_dataset == "matching_to"
 
-    def test_model_allows_mulitiple_records(self):
-        """
-        Test model allows multiple records
-        """
-        person_match_model = PersonMatching(
-            matching_from=MatchingFromPerson(unique_id="1", pnc="1234567890"),
-            matching_to=[MatchingToPerson(unique_id="2", pnc="1234567890")] * 20,
-        )
-        assert len(person_match_model.matching_to) == 20
-
-    def test_model_doesnt_allow_more_than_limit_to_match(self):
-        """
-        Test model doesnt allow more than 100 records
-        """
-        with pytest.raises(ValidationError) as err:
-            PersonMatching(
-                matching_from=MatchingFromPerson(unique_id="1", pnc="1234567890"),
-                matching_to=[MatchingToPerson(unique_id="2", pnc="1234567890")] * 101,
-            )
-        assert err.value.error_count() == 1
-        assert err.value.errors()[0].get("loc") == ("matching_to",)
-        assert err.value.errors()[0].get("msg") == "List should have at most 100 items after validation, not 101"
-
-    def test_model_requires_unique_id(self):
-        """
-        Test model requires unique_id
-        """
-        with pytest.raises(ValidationError) as err:
-            PersonMatching(
-                matching_from=MatchingFromPerson(pnc="1234567890"),
-                matching_to=[MatchingToPerson(pnc="1234567890")] * 51,
-            )
-        assert err.value.error_count() == 1
-        assert err.value.errors()[0].get("loc") == ("unique_id",)
-        assert err.value.errors()[0].get("msg") == "Field required"
