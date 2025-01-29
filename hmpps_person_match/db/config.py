00e8162d1881cb0ab79c3fafb076ea57712d001c
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-01-29 11:41:47 +0000

    CPR-547 Fix typo

--- hmpps_person_match/db/config.py
+++ hmpps_person_match/db/config.py
@@ -20,5 +20,5 @@ class Config:
     DB_CON_POOL_SIZE = 10          # Max connections in the pool
     DB_CON_POOL_MAX_OVERFLOW = 5   # Additional connections allowed beyond pool size
     DB_CON_POOL_TIMEOUT = 30       # Wait time before timeout if pool is full (seconds)
-    DB_CON_POOL_RECYCLE = 1800     # Wiat time before connection is recycled (seconds)
+    DB_CON_POOL_RECYCLE = 1800     # Wait time before connection is recycled (seconds)
     DB_CON_POOL_PRE_PING = True    # Test connections before using them
