72d341640b8c17867275d2fc6333198190d95d09
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-01-29 16:14:24 +0000

    CPR-547 Make db driver configurable with options

--- hmpps_person_match/db/__init__.py
+++ hmpps_person_match/db/__init__.py
@@ -3,27 +3,33 @@ from collections.abc import AsyncGenerator
 from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine, create_async_engine
 
-from hmpps_person_match.db.config import Config
+from hmpps_person_match.db.config import Config, DatabaseDriver
 
-db_query_string_params = None
-if Config.DB_SSL_ENABLED:
-    db_query_string_params = {
-        "ssl": "verify-full",
-    }
 
-# Construct the database URL
-database_url: URL = URL.create(
-    drivername=Config.DB_ASYNC_DRIVER,
-    username=Config.DB_USER,
-    password=Config.DB_PASSWORD,
-    host=Config.DB_HOST,
-    port=Config.DB_PORT,
-    database=Config.DB_NAME,
-    query=db_query_string_params,
-)
+def build_database_url(driver: DatabaseDriver) -> URL:
+    """
+    Build database connection
+    """
+    db_query_string_params = {}
+    if Config.DB_SSL_ENABLED:
+        if driver == DatabaseDriver.ASYNC:
+            db_query_string_params["ssl"] =  "verify-full"
+        if driver == DatabaseDriver.SYNC:
+            db_query_string_params["sslmode"] =  "verify-full"
+
+    # Construct the database URL
+    return URL.create(
+        drivername=driver.value,
+        username=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+        host=Config.DB_HOST,
+        port=Config.DB_PORT,
+        database=Config.DB_NAME,
+        query=db_query_string_params,
+    )
 
 engine: AsyncEngine = create_async_engine(
-    database_url,
+    build_database_url(DatabaseDriver.ASYNC),
     echo=Config.DB_LOGGING,
     pool_size=Config.DB_CON_POOL_SIZE,
     max_overflow=Config.DB_CON_POOL_MAX_OVERFLOW,
