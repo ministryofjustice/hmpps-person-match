aa41d5ce88ec7e1a5f02b5e662904731214ef78a
ADBond <48208438+ADBond@users.noreply.github.com>
2025-01-29 09:50:50 +0000

    CPR-547 - take a pydantic model rather than dict

--- hmpps_cpr_splink/cpr_splink/interface/clean.py
+++ hmpps_cpr_splink/cpr_splink/interface/clean.py
@@ -1,11 +1,13 @@
 import duckdb
 
+from hmpps_person_match.models.person import Person
+
 from ..model_cleaning import create_table_from_records, simple_clean_whole_joined_table
 from ..schemas import DUCKDB_COLUMNS_WITH_TYPES, JoinedRecord
 from .db import insert_duckdb_table_into_postgres_table
 
 
-def clean_and_insert(record: JoinedRecord) -> None:
+def clean_and_insert(record: Person) -> None:
     """
     Takes in a single record in joined format.
 
@@ -13,7 +15,7 @@ def clean_and_insert(record: JoinedRecord) -> None:
     """
     con = duckdb.connect(":memory:")
     record_table_name = "record_table"
-    create_table_from_records(con, [record], record_table_name, DUCKDB_COLUMNS_WITH_TYPES)
+    create_table_from_records(con, [record.model_dump()], record_table_name, DUCKDB_COLUMNS_WITH_TYPES)
 
     t_cleaned = simple_clean_whole_joined_table(record_table_name)
     sql = t_cleaned.create_table_sql
