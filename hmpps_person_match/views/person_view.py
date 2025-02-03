fadba9d505bc5f798597bac31c98ce9b4bfd5599
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-02-03 10:45:52 +0000

    CPR-579 Add test to ensure stored in db correctly

--- hmpps_person_match/views/person_view.py
+++ hmpps_person_match/views/person_view.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Response
@@ -7,6 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncConnection
 from hmpps_cpr_splink.cpr_splink.interface import clean
 from hmpps_person_match.db import get_db_connection
 from hmpps_person_match.dependencies.auth.jwt_bearer import JWTBearer
+from hmpps_person_match.dependencies.logging.log import get_logger
 from hmpps_person_match.domain.constants.openapi.tags import OpenAPITags
 from hmpps_person_match.domain.roles import Roles
 from hmpps_person_match.models.person import Person
@@ -30,9 +32,11 @@ router = APIRouter(
 async def post_person_match(
         person: Person,
         connection: Annotated[AsyncConnection, Depends(get_db_connection)],
+        logger: Annotated[Logger, Depends(get_logger)],
     ) -> Response:
     """
-    Person Match POST request handler
+    Person POST request handler
     """
+    logger.info("Cleaning and storing person record", extra={"custom_dimensions": {"id": person.id}})
     await clean.clean_and_insert(person, connection)
     return JSONResponse(content={}, status_code=200)
