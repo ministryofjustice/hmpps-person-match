416bc13dc7cd140b3302e11ceff42933aa444213
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-02-05 09:31:57 +0000

    CPR-569 Split person routes into different files (#92)
    * CPR-569 Split person routes into different files
    
    * CPR-569 Remove extra description
    
    * CPR-569 Fix int tests

--- hmpps_person_match/routes/person/person_create.py
+++ hmpps_person_match/routes/person/person_create.py
@@ -3,17 +3,14 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Response, status
 from fastapi.responses import JSONResponse
-from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 from hmpps_cpr_splink.cpr_splink.interface import clean
 from hmpps_person_match.db import get_db_connection
 from hmpps_person_match.dependencies.auth.jwt_bearer import JWTBearer
 from hmpps_person_match.dependencies.logging.log import get_logger
-from hmpps_person_match.domain.constants.openapi.tags import OpenAPITags
 from hmpps_person_match.domain.roles import Roles
 from hmpps_person_match.models.person.person import Person
-from hmpps_person_match.models.person.person_identifier import PersonIdentifier
 
 ROUTE = "/person"
 
@@ -24,15 +21,10 @@ DESCRIPTION = f"""
 """
 
 router = APIRouter(
-    tags=[OpenAPITags.PERSON],
-)
-
-
-@router.post(
-    ROUTE,
     dependencies=[Depends(JWTBearer(required_roles=[Roles.ROLE_PERSON_MATCH]))],
-    description=DESCRIPTION,
 )
+
+@router.post(ROUTE, description=DESCRIPTION)
 async def post_person(
     person: Person,
     connection: Annotated[AsyncConnection, Depends(get_db_connection)],
@@ -43,25 +35,4 @@ async def post_person(
     """
     logger.info("Cleaning and storing person record", extra={"custom_dimensions": {"id": person.id}})
     await clean.clean_and_insert(person, connection)
-    return JSONResponse(content={}, status_code=status.HTTP_200_OK)
-
-
-@router.delete(
-    ROUTE,
-    dependencies=[Depends(JWTBearer(required_roles=[Roles.ROLE_PERSON_MATCH]))],
-    description=DESCRIPTION,
-)
-async def delete_person(
-    person_identifier: PersonIdentifier,
-    connection: Annotated[AsyncConnection, Depends(get_db_connection)],
-    logger: Annotated[Logger, Depends(get_logger)],
-) -> Response:
-    """
-    Person DELETE request handler
-    """
-    logger.info("Deleting person record", extra={"custom_dimensions": {"id": person_identifier.match_id}})
-    query = text("DELETE FROM personmatch.person WHERE match_id = :match_id")
-    result = await connection.execute(query, {"match_id": person_identifier.match_id})
-    if result.rowcount == 0:
-        return JSONResponse(content={}, status_code=status.HTTP_404_NOT_FOUND)
     return JSONResponse(content={}, status_code=status.HTTP_200_OK)
