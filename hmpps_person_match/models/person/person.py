9727f0dd39fb0443dfa2f855c4bda1008b3ceef2
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-02-12 10:36:18 +0000

    CPR-593 Allow empty strings in request

--- hmpps_person_match/models/person/person.py
+++ hmpps_person_match/models/person/person.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class Person(BaseModel):
@@ -13,7 +13,7 @@ class Person(BaseModel):
     first_name: str = Field(alias="firstName")
     middle_names: str = Field(alias="middleNames")
     last_name: str = Field(alias="lastName")
-    date_of_birth: datetime.date = Field(alias="dateOfBirth", format="date")  # Ensures YYYY-MM-DD
+    date_of_birth: datetime.date | None = Field(alias="dateOfBirth", format="date", default=None)  # Ensures YYYY-MM-DD
     first_name_aliases: list[str] = Field(alias="firstNameAliases")
     last_name_aliases: list[str] = Field(alias="lastNameAliases")
     date_of_birth_aliases: list[datetime.date] = Field(alias="dateOfBirthAliases", format="date")  # Ensures YYYY-MM-DD
@@ -21,3 +21,10 @@ class Person(BaseModel):
     cros: list[str]
     pncs: list[str]
     sentence_dates: list[datetime.date] = Field(alias="sentenceDates", format="date")  # Ensures YYYY-MM-DD
+
+    @field_validator("date_of_birth", mode="before")
+    @classmethod
+    def validate_date(cls, v):
+        if v == "":
+            return None
+        return v
