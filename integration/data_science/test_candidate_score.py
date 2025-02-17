df830af6c4b8d202a21d40848b3756c896c94e61
ADBond <48208438+ADBond@users.noreply.github.com>
2025-02-17 08:10:49 +0000

    CPR-607 full score function (#105)
    * CPR-607 use match_id for output
    
    still using id 'internally' for blocking
    
    * CPR-607 scoring function returning relevant data
    
    * CPR-607 basic test of scoring function
    
    * CPR-607 use model with tfs but NULL values
    
    flag to decide whether we look up real tables or populate with NULL
    
    * CPR-607 some cleanup
    
    * CPR-607 test scoring nothing
    
    * CPR-607 correct typing
    
    * CPR-607 clarify variables - duckdb vs postgres connexions
    
    * CPR-607 access columns via names rather than positionally

---
+++ integration/data_science/test_candidate_score.py
@@ -0,0 +1,53 @@
+import pytest
+
+from hmpps_cpr_splink.cpr_splink.interface.score import get_scored_candidates
+from hmpps_person_match.models.person.person import Person
+
+
+class TestPersonScore:
+    """
+    Test functioning of candidate search
+    """
+
+    @staticmethod
+    @pytest.fixture(autouse=True, scope="function")
+    async def clean_db(db):
+        """
+        Before Each
+        Delete all records from the database
+        """
+        await db.execute("TRUNCATE TABLE personmatch.person")
+
+    async def test_get_scored_candidates(
+        self,
+        person_id,
+        create_person_record, create_person_data, sqlalchemy_db_connection):
+        """
+        Test retrieving scored candidates gives correct number
+        """
+        # primary record
+        await create_person_record(Person(**create_person_data(person_id)))
+        # candidates - all should match with high match weight
+        n_candidates = 10
+        for _ in range(n_candidates):
+            await create_person_record(Person(**create_person_data()))
+
+        res = await get_scored_candidates(person_id, sqlalchemy_db_connection)
+
+        # we have all candidates + original record
+        assert len(res) == n_candidates
+        assert len([match_weight for r in res if (match_weight := r["candidate_match_weight"]) > 20])
+
+    async def test_get_scored_candidates_none_in_db(
+        self, create_person_record, create_person_data, sqlalchemy_db_connection,
+    ):
+        """
+        Test scoring returns nothing if the given match_id is not in db
+        """
+        n_candidates = 10
+        for _ in range(n_candidates):
+            await create_person_record(Person(**create_person_data()))
+
+        res = await get_scored_candidates("bogus_match_id", sqlalchemy_db_connection)
+
+        assert len(res) == 0
