8e29b39420100f6a7c4a2e3ec4b295fbc5708105
ADBond <48208438+ADBond@users.noreply.github.com>
2025-02-18 12:02:41 +0000

    CPR-577 some tests for term frequencies

---
+++ integration/data_science/test_term_frequencies.py
@@ -0,0 +1,94 @@
+import uuid
+
+import pytest
+from sqlalchemy import text
+
+from hmpps_person_match.models.person.person import Person
+
+
+class TestTFs:
+    """
+    Test term frequency tables
+    """
+
+    def create_person_data(self, first_name: str = "Henry", postcodes: list[str] = None):
+        return {
+            "matchId": str(uuid.uuid4()),
+            "sourceSystem": "DELIUS",
+            "firstName": first_name,
+            "middleNames": "Ahmed",
+            "lastName": "Junaed",
+            "crn": "1234",
+            "dateOfBirth": "1992-03-02",
+            "firstNameAliases": ["Henry"],
+            "lastNameAliases": ["Junaed"],
+            "dateOfBirthAliases": ["1992-01-01"],
+            "postcodes": postcodes if postcodes is not None else ["B10 1EJ"],
+            "cros": ["4444566"],
+            "pncs": ["22224555"],
+            "sentenceDates": ["2001-03-01"],
+        }
+
+
+    @staticmethod
+    @pytest.fixture(autouse=True, scope="function")
+    async def clean_db(db):
+        """
+        Before Each
+        Delete all records from the database
+        """
+        await db.execute("TRUNCATE TABLE personmatch.person")
+
+    async def test_term_frequencies_simple(self, create_person_record, sqlalchemy_db_connection):
+        """
+        Test term frequncies for a simple column
+        """
+        await create_person_record(Person(**self.create_person_data(first_name="Andy")))
+        await create_person_record(Person(**self.create_person_data(first_name="Andy")))
+        await create_person_record(Person(**self.create_person_data(first_name="Andy")))
+        await create_person_record(Person(**self.create_person_data(first_name="Henry")))
+
+        await sqlalchemy_db_connection.execute(
+            text("REFRESH MATERIALIZED VIEW personmatch.term_frequencies_name_1_std;"),
+        )
+        await sqlalchemy_db_connection.commit()
+
+        tf_name_andy = await sqlalchemy_db_connection.execute(
+            text("SELECT tf_name_1_std FROM personmatch.term_frequencies_name_1_std WHERE name_1_std = 'ANDY'"),
+        )
+        assert tf_name_andy.fetchone()[0] == 0.75
+
+        tf_name_henry = await sqlalchemy_db_connection.execute(
+            text("SELECT tf_name_1_std FROM personmatch.term_frequencies_name_1_std WHERE name_1_std = 'HENRY'"),
+        )
+        assert tf_name_henry.fetchone()[0] == 0.25
+
+    async def test_term_frequencies_postcode(self, create_person_record, sqlalchemy_db_connection):
+        """
+        Test term frequencies for an array column
+        """
+        await create_person_record(Person(**self.create_person_data(postcodes=["AB1 1ZY"])))
+        await create_person_record(Person(**self.create_person_data(postcodes=["CD2 2XW"])))
+        await create_person_record(Person(**self.create_person_data(postcodes=["AB1 1ZY", "CD2 2XW"])))
+        await create_person_record(Person(**self.create_person_data(postcodes=["AB1 1ZY", "CD2 2XW"])))
+        await create_person_record(Person(**self.create_person_data(postcodes=["AB1 1ZY", "EF3 3VU"])))
+
+        await sqlalchemy_db_connection.execute(
+            text("REFRESH MATERIALIZED VIEW personmatch.term_frequencies_postcode;"),
+        )
+        await sqlalchemy_db_connection.commit()
+
+        tf_postcode_ab = await sqlalchemy_db_connection.execute(
+            text("SELECT tf_postcode FROM personmatch.term_frequencies_postcode WHERE postcode = 'AB11ZY'"),
+        )
+        assert tf_postcode_ab.fetchone()[0] == 0.5
+
+        tf_postcode_cd = await sqlalchemy_db_connection.execute(
+            text("SELECT tf_postcode FROM personmatch.term_frequencies_postcode WHERE postcode = 'CD22XW'"),
+        )
+        assert tf_postcode_cd.fetchone()[0] == 0.375
+
+        tf_postcode_ef = await sqlalchemy_db_connection.execute(
+            text("SELECT tf_postcode FROM personmatch.term_frequencies_postcode WHERE postcode = 'EF33VU'"),
+        )
+        assert tf_postcode_ef.fetchone()[0] == 0.125
