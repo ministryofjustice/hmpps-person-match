f463b924550c059453345f27943d91cb9e28f90f
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-02-19 11:15:26 +0000

    CPR-79 Increase connection pool limit

--- hmpps_person_match/db/config.py
+++ hmpps_person_match/db/config.py
@@ -17,8 +17,8 @@ class Config:
     DB_SSL_ENABLED = os.environ.get("DB_SSL_ENABLED", "True") == "True"
 
     # Database connection pool settings
-    DB_CON_POOL_SIZE = 10  # Max connections in the pool
+    DB_CON_POOL_SIZE = 20  # Max connections in the pool
     DB_CON_POOL_MAX_OVERFLOW = 5  # Additional connections allowed beyond pool size
     DB_CON_POOL_TIMEOUT = 30  # Wait time before timeout if pool is full (seconds)
-    DB_CON_POOL_RECYCLE = 1800  # Wait time before connection is recycled (seconds)
+    DB_CON_POOL_RECYCLE = 180  # Wait time before connection is recycled (seconds)
     DB_CON_POOL_PRE_PING = True  # Test connections before using them
