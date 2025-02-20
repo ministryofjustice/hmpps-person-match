bbd3c98f19a2df264dd58caa23be792a9ceed928
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-02-20 11:10:58 +0000

    CPR-594 Ensure shutdown releases db resources (#119)

---
+++ hmpps_person_match/lifespan.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from hmpps_person_match.db import engine
+
+
+async def lifespan(app: FastAPI):
+    """
+    Lifespan func of app
+    Disposes of DB engine to release resources
+    """
+    yield
+    engine.dispose()
