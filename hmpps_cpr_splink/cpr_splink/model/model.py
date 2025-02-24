80d9d9aba1a436f756d6ec33e931ddb425c61dbc
ADBond <48208438+ADBond@users.noreply.github.com>
2025-02-24 14:52:26 +0000

    CPR-608 full model, but using only dummy postcode tf values (#126)

--- hmpps_cpr_splink/cpr_splink/model/model.py
+++ hmpps_cpr_splink/cpr_splink/model/model.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-MODEL_VERSION = "model_2025_01_13_5e08_no_postcode"
+MODEL_VERSION = "model_2025_01_28_1e09"
 
 MODEL_PATH = Path(__file__).parent / f"model_files/{MODEL_VERSION}.json"
 # TODO: other metadata of use to library client?
