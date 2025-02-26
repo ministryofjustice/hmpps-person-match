a232904aabbd86e0fce6cce99e60bffcee8534be
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-02-26 14:13:12 +0000

    CPR-577 Fix error on generating term frequency

--- integration/endpoints/test_person_score.py
+++ integration/endpoints/test_person_score.py
@@ -4,6 +4,7 @@ import pytest
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from hmpps_person_match.models.person.person import Person
 from hmpps_person_match.routes.person.score.person_score import ROUTE
 from integration.client import Client
 
@@ -54,26 +55,20 @@ class TestPersonScoreEndpoint:
         assert response.status_code == 200
         assert response.json() == []
 
-    async def test_score_returns_candidates(self, call_endpoint, match_id, create_person_data):
+    async def test_score_returns_candidates(self, call_endpoint, match_id, create_person_data, create_person_record):
         """
         Test person cleaned and stored on person endpoint
         """
         # Create person to match and score
-        data = create_person_data(match_id)
-        response = call_endpoint("post", "/person", json=data, client=Client.HMPPS_PERSON_MATCH)
-        assert response.status_code == 200
+        await create_person_record(Person(**create_person_data(match_id)))
 
         # Create different person
         matching_person_id_1 = str(uuid.uuid4())
-        data = create_person_data(matching_person_id_1)
-        response = call_endpoint("post", "/person", json=data, client=Client.HMPPS_PERSON_MATCH)
-        assert response.status_code == 200
+        await create_person_record(Person(**create_person_data(matching_person_id_1)))
 
         # Create different person
         matching_person_id_2 = str(uuid.uuid4())
-        data = create_person_data(matching_person_id_2)
-        response = call_endpoint("post", "/person", json=data, client=Client.HMPPS_PERSON_MATCH)
-        assert response.status_code == 200
+        await create_person_record(Person(**create_person_data(matching_person_id_2)))
 
         # Call score for person
         response = call_endpoint("get", self._build_score_url(match_id), client=Client.HMPPS_PERSON_MATCH)
