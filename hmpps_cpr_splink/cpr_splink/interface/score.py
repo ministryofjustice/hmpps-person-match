62cd324b6b404d92fa814fbdcc623535707715c2
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-03-14 16:08:45 +0000

    CPR-629 Ensure duckdb connection is closed after use

--- hmpps_cpr_splink/cpr_splink/interface/score.py
+++ hmpps_cpr_splink/cpr_splink/interface/score.py
@@ -25,27 +25,26 @@ async def get_scored_candidates(
     Takes a primary record, generates candidates, scores
     """
     # TODO: allow a threshold cutoff? (depending on blocking rules)
-    connection_duckdb = duckdb_connected_to_postgres(pg_db_url)
+    with duckdb_connected_to_postgres(pg_db_url) as connection_duckdb:
+        candidates_data = await candidate_search(primary_record_id, connection_pg)
 
-    candidates_data = await candidate_search(primary_record_id, connection_pg)
+        if not candidates_data:
+            return []
+        candidates_table_name = "candidates"
 
-    if not candidates_data:
-        return []
-    candidates_table_name = "candidates"
+        create_table_from_records(connection_duckdb, candidates_data, candidates_table_name, CLEANED_TABLE_SCHEMA)
 
-    create_table_from_records(connection_duckdb, candidates_data, candidates_table_name, CLEANED_TABLE_SCHEMA)
+        res = score(connection_duckdb, primary_record_id, candidates_table_name, return_scores_only=True)
 
-    res = score(connection_duckdb, primary_record_id, candidates_table_name, return_scores_only=True)
-
-    data = [dict(zip(res.columns, row, strict=True)) for row in res.fetchall()]
-    return [
-        {
-            "candidate_match_id": row["match_id_r"],  # match_id_l is primary record
-            "candidate_match_probability": row["match_probability"],
-            "candidate_match_weight": row["match_weight"],
-        }
-        for row in data
-    ]
+        data = [dict(zip(res.columns, row, strict=True)) for row in res.fetchall()]
+        return [
+            {
+                "candidate_match_id": row["match_id_r"],  # match_id_l is primary record
+                "candidate_match_probability": row["match_probability"],
+                "candidate_match_weight": row["match_weight"],
+            }
+            for row in data
+        ]
 
 
 async def match_record_exists(match_id: str, connection_pg: AsyncSession) -> bool:
