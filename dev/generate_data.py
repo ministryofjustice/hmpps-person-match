b1f66490785ddd2729b8f2b1855d4ba96002fa63
ADBond <48208438+ADBond@users.noreply.github.com>
2025-03-20 14:02:57 +0000

    CPR-638 remove redundant column (#181)
    * CPR-638 drop doubled column
    
    * CPr-638 remove reference to sentence_date_single
    
    * CPR-638 fix cleaning expression
    
    * CPR-638 update generated data
    
    * CPR-638 correct dump command

--- dev/generate_data.py
+++ dev/generate_data.py
@@ -9,7 +9,17 @@ fake = Faker(["en_GB"])
 Faker.seed(4321)
 
 
+def stringify(datum):
+    if isinstance(datum, datetime):
+        return datum.strftime("%Y-%m-%d")
+    return str(datum)
+
 def make_person(id_val):
+    sentence_dates = [
+        fake.date_between(datetime(1990, 1, 1), datetime(2025, 3, 1)),  # noqa: DTZ001
+        fake.date_between(datetime(1990, 1, 1), datetime(2025, 3, 1)),  # noqa: DTZ001
+    ]
+    sentence_dates.sort()
     cleaned_person = {
         "id": id_val,
         "match_id": fake.uuid4(),
@@ -17,7 +27,7 @@ def make_person(id_val):
         "name_2_std": fake.first_name().upper(),
         "name_3_std": fake.first_name().upper(),
         "last_name_std": fake.last_name().upper(),
-        "sentence_date_single": fake.date_between(datetime(1990, 1, 1), datetime(2025, 3, 1)),  # noqa: DTZ001
+        "sentence_date_arr": sentence_dates,
         "date_of_birth": fake.date_between(datetime(1940, 1, 1), datetime(2007, 3, 1)),  # noqa: DTZ001
         "postcode_arr": [fake.postcode(), fake.postcode(), fake.postcode()],
         "cro_single": str(fake.random_int(min=1, max=1_000_000)),  # fake.passport_number(),
@@ -26,17 +36,32 @@ def make_person(id_val):
         "prison_number": None,
         "source_system": None,
     }
+    # NB: not completely consistent with real data - no duplicates, arrays not sorted
     cleaned_person["first_and_last_name_std"] = cleaned_person["name_1_std"] + " " + cleaned_person["last_name_std"]
-    cleaned_person["forename_std_arr"] = [cleaned_person["name_1_std"], cleaned_person["name_2_std"]]
-    cleaned_person["last_name_std_arr"] = [cleaned_person["last_name_std"]]
-    cleaned_person["sentence_date_arr"] = [cleaned_person["sentence_date_single"].strftime("%Y-%m-%d")]
+    cleaned_person["forename_std_arr"] = [
+        cleaned_person["name_1_std"], cleaned_person["name_2_std"], fake.first_name().upper(),
+    ]
+    cleaned_person["last_name_std_arr"] = [cleaned_person["last_name_std"], fake.last_name().upper()]
     cleaned_person["date_of_birth_arr"] = [cleaned_person["date_of_birth"].strftime("%Y-%m-%d")]
     cleaned_person["postcode_outcode_arr"] = list(map(lambda pc: pc.split(" ")[0], cleaned_person["postcode_arr"]))
     cleaned_person["postcode_arr"] = list(map(lambda pc: pc.replace(" ", ""), cleaned_person["postcode_arr"]))
+    # derived columns
+    cleaned_person["sentence_date_first"] = sentence_dates[0]
+    cleaned_person["sentence_date_last"] = sentence_dates[-1]
+    cleaned_person["postcode_first"] = cleaned_person["postcode_arr"][0]
+    cleaned_person["postcode_second"] = cleaned_person["postcode_arr"][1]
+    cleaned_person["postcode_last"] = cleaned_person["postcode_arr"][-1]
+    cleaned_person["postcode_outcode_first"] = cleaned_person["postcode_outcode_arr"][0]
+    cleaned_person["postcode_outcode_last"] = cleaned_person["postcode_outcode_arr"][-1]
+    cleaned_person["date_of_birth_last"] = cleaned_person["date_of_birth_arr"][-1]
+    cleaned_person["forename_first"] = cleaned_person["forename_std_arr"][0]
+    cleaned_person["forename_last"] = cleaned_person["forename_std_arr"][-1]
+    cleaned_person["last_name_first"] = cleaned_person["last_name_std_arr"][0]
+    cleaned_person["last_name_last"] = cleaned_person["last_name_std_arr"][-1]
 
     for col in cleaned_person.keys():  # NOQA
         if isinstance(cleaned_person[col], list):
-            cleaned_person[col] = "[" + ", ".join(cleaned_person[col]) + "]"
+            cleaned_person[col] = "[" + ", ".join(map(stringify, cleaned_person[col])) + "]"
 
     return cleaned_person
 
