94160db2b4fbc49bc7efdae5b648cdf391059bb2
Robin Linacre <robinlinacre@hotmail.com>
2025-05-06 10:15:31 +0100

    Verify model training script gives right result
    (Check that the model training script yields the same parameters
    as the model that's checked in)

--- hmpps_cpr_splink/run_training.py
+++ hmpps_cpr_splink/run_training.py
@@ -1,138 +1,86 @@
-# noqa: INP001
-import os
-from datetime import datetime
+from datetime import datetime  # noqa: INP001
 
 import duckdb
-from splink import DuckDBAPI, Linker
+from IPython.display import display
+from splink import DuckDBAPI
 
-from hmpps_cpr_splink.cpr_splink.data_cleaning.join_raw_tables import join_raw_tables_sql
-from hmpps_cpr_splink.cpr_splink.model_cleaning import clean_whole_joined_table
-from hmpps_cpr_splink.cpr_splink.model_cleaning.tables import create_postcode_tf_from_cpr_joined
+from hmpps_cpr_splink.cpr_splink.data_cleaning.table import Table
+from hmpps_cpr_splink.cpr_splink.model import ModelTraining
+from hmpps_cpr_splink.cpr_splink.model_cleaning.term_frequencies import (
+    lookup_term_frequencies,
+)
 
-joined_data_exists = False
-test_con = duckdb.connect()
-
-
-try:
-    test_con.execute("SELECT * FROM read_parquet('secret_data/cleaned/joined/*.parquet') LIMIT 1")
-    joined_data_exists = True
-except duckdb.IOException:
-    joined_data_exists = False
-finally:
-    test_con.close()
-
-if not joined_data_exists:
-    con_join = duckdb.connect()
-    sql = join_raw_tables_sql(
-        dict(
-            person_in="read_parquet('secret_data/raw/person/*.parquet')",
-            pseudonym_in="read_parquet('secret_data/raw/pseudonym/*.parquet')",
-            address_in="read_parquet('secret_data/raw/address/*.parquet')",
-            reference_in="read_parquet('secret_data/raw/reference/*.parquet')",
-            sentence_info_in="read_parquet('secret_data/raw/sentence_info/*.parquet')",
-        ),
-    )
-
-    df = con_join.sql(sql)
-    os.makedirs("secret_data/cleaned/joined/", exist_ok=True)
-
-    con_join.execute(
-        """
-        COPY df
-        TO './secret_data/cleaned/joined/'
-        (FORMAT PARQUET, PER_THREAD_OUTPUT, ROW_GROUP_SIZE 100000);
-    """,
-    )
-    con_join.close()
-else:
-    print("Joined data files already exist, skipping processing")  # NOQA: T201
-
-cleaned_data_exists = False
-test_con = duckdb.connect()
-
-try:
-    test_con.execute("SELECT * FROM read_parquet('secret_data/cleaned/cleaned/*.parquet') LIMIT 1")
-    cleaned_data_exists = True
-except duckdb.IOException:
-    cleaned_data_exists = False
-finally:
-    test_con.close()
-
-if not cleaned_data_exists:
-    con_clean = duckdb.connect()
-
-    sql = """
-    create or replace table df_joined as
-    select *
-    from read_parquet('secret_data/cleaned/joined/*.parquet')
-    """
-    con_clean.execute(sql)
-
-    sql = create_postcode_tf_from_cpr_joined("df_joined").create_table_sql
-    con_clean.sql(sql)
-
-    sql = clean_whole_joined_table("df_joined", "tf_postcodes").create_table_sql
-    con_clean.sql(sql)
-
-    os.makedirs("secret_data/cleaned/cleaned/", exist_ok=True)
-
-    con_clean.execute(
-        """
-        COPY df_cleaned_with_arr_freq
-        TO './secret_data/cleaned/cleaned/'
-        (FORMAT PARQUET, PER_THREAD_OUTPUT, ROW_GROUP_SIZE 100000);
-    """,
-    )
-    con_clean.close()
-else:
-    print("Cleaned data files already exist, skipping processing")  # NOQA: T201
-
-
-con_training = duckdb.connect()
+con = duckdb.connect()
 
 sql = """
-create or replace table df_training as
+create or replace table personmatch_person as
 select *
-from read_parquet('secret_data/cleaned/cleaned/*.parquet')
+from read_parquet('hmpps_cpr_splink/secret_data/personmatch/raw/person/*.parquet')
 
 """
+con.execute(sql)
 
-con_training.execute(sql)
-
-
-db_api = DuckDBAPI(con_training)
+con.table("personmatch_person").show(max_width=10000)
 
 
-train_u_size = 1e9
-date_str = datetime.now().astimezone().strftime("%Y_%m_%d")
+# Note that the cleaned data doesn't have postcode frequencies so we need to
+# derive this as part of training
+pc_distinct_unnested = Table(
+    "pc_unnested",
+    "UNNEST(postcode_arr) AS value",
+    from_table="personmatch_person",
+)
+
+
+pc_frequencies = Table(
+    "tf_postcodes",
+    "value",
+    "COUNT(*) as freq",
+    "COUNT(*) * 1.0 / (SELECT COUNT(*) FROM pc_unnested) as rel_freq",
+    from_table=pc_distinct_unnested,
+    post_from_clauses="GROUP BY value",
+)
+
+
+con.execute(pc_frequencies.create_table_sql)
+
+
+t_postcode_with_freq = lookup_term_frequencies(
+    "postcode_arr",
+    "tf_postcodes",
+    "personmatch_person",
+)
+
+
+t_postcode_final = Table(
+    "cleaned_with_postcode_tfs",
+    "personmatch_person.*",
+    "agg_table_postcode_arr.postcode_arr_with_freq",
+    from_table=t_postcode_with_freq,
+    post_from_clauses=(
+        f"RIGHT JOIN personmatch_person ON personmatch_person.match_id = {t_postcode_with_freq}.match_id"
+    ),
+)
+
+
+con.execute(t_postcode_final.create_table_sql)
+con.table("cleaned_with_postcode_tfs").show(max_width=10000)
+
+assert (  # noqa: S101
+    con.table("cleaned_with_postcode_tfs").count("*").fetchone()[0]
+    == con.table("personmatch_person").count("*").fetchone()[0]
+), "The number of rows in the cleaned table should match the original table"
+
+db_api = DuckDBAPI(con)
+
+
+train_u_size = 1e8
+date_str = datetime.now().strftime("%Y_%m_%d")  # noqa: DTZ005
 train_u_size_str = f"{train_u_size:.0e}".replace("+", "")
 model_name = f"model_{date_str}_{train_u_size_str}.json"
 
-linker = Linker("df_training", "model_2025_01_13_5e08.json", db_api)
+linker = ModelTraining(train_u_size=train_u_size).train("cleaned_with_postcode_tfs", db_api)
 
-df_predict = linker.inference.predict(threshold_match_weight=20.0)
+display(linker.visualisations.match_weights_chart())
 
-con_training.execute(
-    f"""
-    COPY {df_predict.physical_name}
-    TO './secret_data/cleaned/predictions/'
-    (FORMAT PARQUET, PER_THREAD_OUTPUT, ROW_GROUP_SIZE 100000);
-""",
-)
-
-
-linker.visualisations.match_weights_histogram(df_predict)
-linker.visualisations.comparison_viewer_dashboard(df_predict, "secret_data/cleaned/comparison_viewer/cv.html")
-
-clustered = linker.clustering.cluster_pairwise_predictions_at_threshold(df_predict, threshold_match_weight=20.0)
-clustered.as_duckdbpyrelation().show(max_width=1000)
-cluster_ddb = clustered.as_duckdbpyrelation()
-
-
-con_training.execute(
-    f"""
-    COPY {clustered.physical_name}
-    TO './secret_data/cleaned/clustered/'
-    (FORMAT PARQUET, PER_THREAD_OUTPUT, ROW_GROUP_SIZE 100000);
-""",
-)
+linker.misc.save_model_to_json("hmpps_cpr_splink/cpr_splink/model/model_files/" + model_name, overwrite=True)
