fa5c1ce642a98dd4432a32ad9845dfba17a9ddc9
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-05-08 10:39:47 +0100

    CPR-669 Fix downgrade (#222)
    * CPR-669 Fix downgrade
    
    * CPR-669 Remove upgrade logic

---
+++ hmpps_person_match/db/migrations/versions/9b60b9a50127_add_source_system_id_column.py
@@ -0,0 +1,35 @@
+"""add source system id column
+Revision ID: 9b60b9a50127
+Revises: 38d705884365
+Create Date: 2025-05-07 13:56:43.984089
+"""
+
+from collections.abc import Sequence
+
+import sqlalchemy
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "9b60b9a50127"
+down_revision: str | None = "38d705884365"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+   pass
+
+
+def downgrade() -> None:
+    op.drop_constraint(constraint_name="uq_source_system_id", table_name="person", schema="personmatch")
+    op.drop_column(table_name="person", column_name="source_system_id", schema="personmatch")
+    op.add_column(
+        table_name="person",
+        column=sqlalchemy.Column("crn", sqlalchemy.Text),
+        schema="personmatch",
+    )
+    op.add_column(
+        table_name="person",
+        column=sqlalchemy.Column("prison_number", sqlalchemy.Text),
+        schema="personmatch",
+    )
