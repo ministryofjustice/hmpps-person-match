05c325923a6bcf280723f5562f98dbb0140ae90f
samz-justice <sam.zaroug@justice.gov.uk>
2025-05-23 10:59:53 +0100

    CPR-622 unit tests for record count report endpoint

--- hmpps_person_match/routes/jobs/record_count_report.py
+++ hmpps_person_match/routes/jobs/record_count_report.py
@@ -1,35 +1,38 @@
 from logging import Logger
-from typing import Annotated
+from typing import Annotated, TypedDict
 
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from hmpps_person_match.db import get_db_session
-from hmpps_person_match.dependencies.auth.jwt_bearer import JWTBearer
 from hmpps_person_match.dependencies.logger.log import get_logger
-from hmpps_person_match.domain.roles import Roles
+from hmpps_person_match.domain.telemetry_events import TelemetryEvents
 
 ROUTE = "/jobs/recordcountreport"
 
-DESCRIPTION = f"""
-    **Authorization Required:**
-    - Bearer Token must be provided.
-    - Role: **'{Roles.ROLE_PERSON_MATCH}'**
-"""
-
-router = APIRouter(
-    dependencies=[Depends(JWTBearer(required_roles=[Roles.ROLE_PERSON_MATCH]))],
-)
+router = APIRouter()
 
 
-@router.get(ROUTE, description=DESCRIPTION)
+class RecordCountReport(TypedDict):
+    source_system: str
+    count: int
+
+
+@router.get(ROUTE, include_in_schema=False)
 async def get_record_count_report(
     session: Annotated[AsyncSession, Depends(get_db_session)],
     logger: Annotated[Logger, Depends(get_logger)],
-) -> Response:
+) -> JSONResponse:
     """
     Record count report GET request handler
     Returns the count of person record
     """
+    result = await session.execute(
+        text("select p.source_system, count(*) from personmatch.person p group by p.source_system"),
+    )
+    result_mapping_dict = {row["source_system"]: row["count"] for row in result.mappings().fetchall()}
+    logger.info(TelemetryEvents.JOBS_MATCH_RECORD_COUNT_REPORT, extra=result_mapping_dict)
+
     return JSONResponse(content={}, status_code=status.HTTP_200_OK)
---
+++ hmpps_person_match/routes/jobs/tests/test_record_count_report.py
@@ -0,0 +1,71 @@
+from unittest.mock import Mock
+
+from hmpps_person_match.domain.telemetry_events import TelemetryEvents
+from hmpps_person_match.routes.jobs.record_count_report import ROUTE
+
+
+class TestRecordCountReportEndPoint:
+    """
+    Test record count report endpoint
+    """
+
+    async def test_no_records_found_report(
+        self,
+        call_endpoint,
+        mock_db_connection,
+        mock_logger,
+    ):
+        """
+        Test when record count report is empty
+        """
+        mock_db_result = Mock()
+        mock_db_result.mappings().fetchall.return_value = []
+        mock_db_connection.execute.return_value = mock_db_result
+
+        # Call record count report endpoint
+        response = call_endpoint("get", ROUTE)
+        assert response.status_code == 200
+
+        mock_logger.info.assert_called_with(
+            TelemetryEvents.JOBS_MATCH_RECORD_COUNT_REPORT,
+            extra={},
+        )
+
+    async def test_record_count_report(
+        self,
+        call_endpoint,
+        mock_db_connection,
+        mock_logger,
+    ):
+        """
+        Test record count report
+        """
+        mock_db_result = Mock()
+        mock_db_result.mappings().fetchall.return_value = [
+            {
+                "source_system": "LIBRA",
+                "count": 2,
+            },
+            {
+                "source_system": "COMMON_PLATFORM",
+                "count": 200,
+            },
+            {
+                "source_system": "NOMIS",
+                "count": 100,
+            },
+            {
+                "source_system": "DELIUS",
+                "count": 5,
+            },
+        ]
+        mock_db_connection.execute.return_value = mock_db_result
+
+        # Call record count report endpoint
+        response = call_endpoint("get", ROUTE)
+        assert response.status_code == 200
+
+        mock_logger.info.assert_called_with(
+            TelemetryEvents.JOBS_MATCH_RECORD_COUNT_REPORT,
+            extra={"LIBRA": 2, "COMMON_PLATFORM": 200, "NOMIS": 100, "DELIUS": 5},
+        )
