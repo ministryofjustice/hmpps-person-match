eaed7c0ae168a75ef887550056c9d8dbccc2abe3
Robin Linacre <robinlinacre@hotmail.com>
2025-08-19 10:48:22 +0100

    allow null

--- hmpps_person_match/models/person/person.py
+++ hmpps_person_match/models/person/person.py
@@ -37,8 +37,11 @@ class Person(BaseModel):
     )  # Ensures YYYY-MM-DD
 
     # TOD: Does this need | None?
-    manual_override: str = Field(alias="manualOverride", examples=["ec30e2d2-b4c2-4c42-9e14-514aa58edff5"])
-    override_scopes: list[str] = Field(alias="overrideScopes", examples=[["dk38987d-skd8-d077-knd9-d0983ld9dfj8"]])
+    manual_override: str | None = Field(alias="manualOverride", examples=["ec30e2d2-b4c2-4c42-9e14-514aa58edff5"])
+    override_scopes: list[str] | None = Field(
+        alias="overrideScopes",
+        examples=[["dk38987d-skd8-d077-knd9-d0983ld9dfj8"]],
+    )
 
     @field_validator("date_of_birth", mode="before")
     @classmethod
