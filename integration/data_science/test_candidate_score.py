85412a156226ac52ac37869ff50d9731e4240fc2
ADBond <48208438+ADBond@users.noreply.github.com>
2025-09-01 17:40:55 +0100

    test that different markers without scope overlap leave score unaffected

--- integration/data_science/test_candidate_score.py
+++ integration/data_science/test_candidate_score.py
@@ -238,3 +238,57 @@ class TestPersonScore(IntegrationTestBase):
 
         assert len(scored_candidates) == 1
         assert scored_candidates[0].candidate_match_weight < -100
+
+    async def test_score_is_unaffected_when_overrides_differ_in_different_scopes(
+        self,
+        create_person_record,
+        pg_db_url,
+        db_connection,
+    ):
+        """
+        Tests that if two records have different override_markers _and_ no overlapping
+        override_scopes, their match weight is unaffected.
+        """
+        #  Guarantee pair caught by candidate search by using same PNC
+        shared_pnc = random_test_data.random_pnc()
+
+        # create two different people, and capture score
+        person_1_id = str(uuid.uuid4())
+        person_2_id = str(uuid.uuid4())
+
+        person_1 = MockPerson(
+            matchId=person_1_id,
+            firstName="Baseline",
+            lastName="Person",
+            dateOfBirth="1990-01-01",
+            pncs=[shared_pnc],
+        )
+        person_2 = MockPerson(
+            matchId=person_2_id,
+            firstName="Baseline",
+            lastName="Person",
+            dateOfBirth="1991-02-02",
+            pncs=[shared_pnc],
+        )
+        await create_person_record(person_1)
+        await create_person_record(person_2)
+
+        scored_candidates = await get_scored_candidates(person_1_id, pg_db_url, db_connection)
+        scored_candidates_weight = scored_candidates[0].candidate_match_weight
+
+        # Should be no manual override, as it's null
+        assert -40 < scored_candidates_weight < 40
+
+        # update people to have different override markers with no overlapping scope
+        person_1.override_marker = uuid.uuid4()
+        person_2.override_marker = uuid.uuid4()
+        person_1.override_scopes = [uuid.uuid4()]
+        person_2.override_scopes = [uuid.uuid4()]
+
+        await create_person_record(person_1)
+        await create_person_record(person_2)
+
+        scored_candidates = await get_scored_candidates(person_1_id, pg_db_url, db_connection)
+        new_scored_candidates_weight = scored_candidates[0].candidate_match_weight
+
+        assert scored_candidates_weight == new_scored_candidates_weight
