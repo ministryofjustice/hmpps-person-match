212f7a70949789f649e6515cf9398432aa1ba782
Robin Linacre <robinlinacre@hotmail.com>
2025-09-22 13:31:45 +0100

    move spec to json

--- hmpps_cpr_splink/cpr_splink/visualisation/visualise_cluster_spec.py
+++ hmpps_cpr_splink/cpr_splink/visualisation/visualise_cluster_spec.py
@@ -1,5 +1,8 @@
-from copy import deepcopy
+import json
+from pathlib import Path
 from typing import Any
+
+MODEL_PATH = Path(__file__).parent / "specs/force_directed.json"
 
 NODE_TOOLTIP_FIELDS = [
     "id",
@@ -37,193 +40,15 @@ EDGE_TOOLTIP_SIGNAL = (
     "{" + ", ".join(f'"{field}": datum.{field}_l + " <-> " + datum.{field}_r' for field in EDGE_COMPARISON_FIELDS) + "}"
 )
 
-# see https://www.robinlinacre.com/microblog/#different-ways-of-setting-out-data-in-a-vega-force-directed-layout
-BASE_SPEC: dict[str, Any] = {
-    "$schema": "https://vega.github.io/schema/vega/v6.json",
-    "width": 500,
-    "height": 500,
-    "signals": [
-        {"name": "$cx", "update": "width / 2"},
-        {"name": "$cy", "update": "height / 2"},
-        {"name": "$nodeRadius", "value": 22},
-        {"name": "$nodeCharge", "value": -15},
-        {"name": "$linkDistance", "value": 140},
-        {"name": "$static", "value": False},
-        {
-            "name": "scoreThreshold",
-            "value": 18,
-            "bind": {
-                "input": "range",
-                "min": -60,
-                "max": 60,
-                "step": 1,
-                "name": "Score Threshold:",
-            },
-        },
-        {
-            "description": "State variable for active node fix status.",
-            "name": "fix",
-            "value": False,
-            "on": [
-                {
-                    "events": "symbol:pointerout[!event.buttons], window:pointerup",
-                    "update": "false",
-                },
-                {"events": "symbol:pointerover", "update": "fix || true"},
-                {
-                    "events": "[symbol:pointerdown, window:pointerup] > window:pointermove!",
-                    "update": "xy()",
-                    "force": True,
-                },
-            ],
-        },
-        {
-            "description": "Graph node most recently interacted with.",
-            "name": "node",
-            "value": None,
-            "on": [
-                {
-                    "events": "symbol:pointerover",
-                    "update": "fix === true ? item() : node",
-                },
-            ],
-        },
-        {
-            "description": "Flag to restart force simulation when drag state changes.",
-            "name": "restart",
-            "value": 0,
-            "on": [
-                {"events": {"signal": "fix"}, "update": "restart + 1"},
-                {"events": {"signal": "scoreThreshold"}, "update": "restart + 1"},
-            ],
-        },
-    ],
-    "data": [
-        {"name": "node-data", "values": [{"source_system": 1, "match_id": "a"}, {"source_system": 1, "match_id": "b"}]},
-        {"name": "link-data", "values": [{"source": "a", "target": "b", "weight": 50.784104988004216}]},
-    ],
-    "scales": [
-        {
-            "name": "color",
-            "type": "ordinal",
-            "domain": {"data": "node-data", "field": "source_system"},
-            "range": {"scheme": "category10"},
-        },
-        {"name": "linkColor", "type": "threshold", "domain": [18, 24], "range": ["#fb9191", "#fdbe7a", "#9fff9f"]},
-    ],
-    "marks": [
-        {
-            "name": "nodes",
-            "type": "symbol",
-            "zindex": 1,
-            "from": {"data": "node-data"},
-            "on": [
-                {
-                    "trigger": "fix",
-                    "modify": "node",
-                    "values": "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}",
-                },
-                {"trigger": "!fix", "modify": "node", "values": "{fx: null, fy: null}"},
-            ],
-            "encode": {
-                "enter": {"fill": {"scale": "color", "field": "source_system"}, "stroke": {"value": "white"}},
-                "update": {
-                    "size": {"signal": "2 * $nodeRadius * $nodeRadius"},
-                    "cursor": {"value": "pointer"},
-                    "tooltip": {"signal": NODE_TOOLTIP_SIGNAL},
-                },
-            },
-            "transform": [
-                {
-                    "type": "force",
-                    "iterations": 300,
-                    "restart": {"signal": "restart"},
-                    "static": {"signal": "$static"},
-                    "signal": "force",
-                    "forces": [
-                        {"force": "center", "x": {"signal": "$cx"}, "y": {"signal": "$cy"}},
-                        {"force": "collide", "radius": {"signal": "$nodeRadius"}},
-                        {"force": "nbody", "strength": {"signal": "$nodeCharge"}},
-                        {
-                            "force": "link",
-                            "id": "datum.match_id",
-                            "links": "link-data",
-                            "distance": {"signal": "$linkDistance"},
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            "type": "text",
-            "name": "node-labels",
-            "from": {"data": "nodes"},
-            "interactive": False,
-            "zindex": 2,
-            "encode": {
-                "enter": {
-                    "align": {"value": "center"},
-                    "baseline": {"value": "middle"},
-                    "fontSize": {"value": 12},
-                    "fontWeight": {"value": "bold"},
-                    "fill": {"value": "white"},
-                    "text": {"signal": "slice(datum.datum.match_id, 0, 4)"},
-                },
-                "update": {
-                    "x": {"field": "x"},
-                    "y": {"field": "y"},
-                },
-            },
-        },
-        {
-            "type": "text",
-            "name": "link-labels",
-            "from": {"data": "link-data"},
-            "interactive": False,
-            "zindex": 1,
-            "encode": {
-                "enter": {
-                    "align": {"value": "center"},
-                    "baseline": {"value": "middle"},
-                    "fontSize": {"value": 12},
-                    "fill": {"value": "black"},
-                    "fontWeight": {"value": "bold"},
-                },
-                "update": {
-                    "text": {"signal": "format(datum.match_weight, '.1f')"},
-                    "x": {"signal": "(datum.source.x + datum.target.x) / 2"},
-                    "y": {"signal": "(datum.source.y + datum.target.y) / 2"},
-                },
-            },
-        },
-        {
-            "type": "path",
-            "from": {"data": "link-data"},
-            "interactive": True,
-            "encode": {
-                "update": {
-                    "stroke": {"scale": "linkColor", "field": "match_weight"},
-                    "strokeWidth": {"value": 3},
-                    "tooltip": {"signal": EDGE_TOOLTIP_SIGNAL},
-                },
-            },
-            "transform": [
-                {
-                    "type": "linkpath",
-                    "require": {"signal": "force"},
-                    "shape": "line",
-                    "sourceX": "datum.source.x",
-                    "sourceY": "datum.source.y",
-                    "targetX": "datum.target.x",
-                    "targetY": "datum.target.y",
-                },
-            ],
-        },
-    ],
-}
-
 
 def load_base_spec() -> dict[str, Any]:
     """Return a deep copy of the base Vega spec so callers can mutate safely."""
 
-    return deepcopy(BASE_SPEC)
+    # see https://www.robinlinacre.com/microblog/#different-ways-of-setting-out-data-in-a-vega-force-directed-layout
+    with open(MODEL_PATH) as f:
+        base_spec = json.load(f)
+
+    base_spec["marks"][0]["encode"]["update"]["tooltip"]["signal"] = NODE_TOOLTIP_SIGNAL
+    base_spec["marks"][3]["encode"]["update"]["tooltip"]["signal"] = EDGE_TOOLTIP_SIGNAL
+
+    return base_spec
