07674813fddfacb381d55fc000f7b40618a80380
ADBond <48208438+ADBond@users.noreply.github.com>
2025-10-02 17:40:26 +0100

    twin-detection column - just metadata for now

--- hmpps_cpr_splink/cpr_splink/model/score.py
+++ hmpps_cpr_splink/cpr_splink/model/score.py
@@ -3,11 +3,76 @@ import time
 
 import duckdb
 from splink import DuckDBAPI
+from splink.internals.pipeline import CTEPipeline
 from splink.internals.realtime import compare_records
 
 from .model import MODEL_PATH
 
 logger = logging.getLogger(__name__)
+
+
+def filter_twins_sql(table_name: str) -> str:
+    pipeline = CTEPipeline()
+    jw_thresh = 0.99
+
+    twins_condition = f"""
+        -- first name not a match
+            name_1_std_l <> name_1_std_r
+        -- every pair of aliases is dissimilar
+        AND
+            list_aggregate(
+                list_transform(
+                    list_transform(
+                        forename_std_arr_l,
+                        alias_l -> list_transform(
+                            forename_std_arr_r,
+                            alias_r -> jaro_winkler_similarity(alias_l, alias_r) < {jw_thresh}
+                        )
+                    ),
+                    is_dissimilar_arr -> list_aggregate(
+                        is_dissimilar_arr,
+                        'bool_and'
+                    )
+                ),
+                'bool_and'
+            )
+        -- dob matchess
+        AND
+            date_of_birth_l = date_of_birth_r
+        -- surname matches
+        AND
+            last_name_std_l = last_name_std_r
+        -- either sentenced on same date, or postcode in common
+        AND (
+            array_length(array_intersect(sentence_date_arr_r, sentence_date_arr_l)) > 0
+            OR
+            array_length(array_intersect(postcode_arr_r, postcode_arr_l)) > 0
+        )
+        -- no matching ID of either type
+        AND
+            ifnull(cro_single_l, 'cro_l') <> ifnull(cro_single_r, 'cro_r')
+        AND
+            ifnull(pnc_single_l, 'pnc_l') <> ifnull(pnc_single_r, 'pnc_r')
+        -- look sufficiently similar
+        AND
+            match_weight > 8
+    """
+
+    sql_filter_dob = {
+        "sql": f"""
+            SELECT
+                *,
+                (
+                    {twins_condition}
+                ) AS possible_twins
+            FROM
+                {table_name}
+        """,  # noqa: S608
+        "output_table_name": "scored_same_birthdate_no_id_match",
+    }
+    pipeline.enqueue_list_of_sqls([sql_filter_dob])
+
+    return pipeline.generate_cte_pipeline_sql()
 
 
 def score(
@@ -32,18 +97,23 @@ def score(
     connection_duckdb.execute(source_sql, parameters={"primary_record_id": primary_record_id})
     connection_duckdb.execute(candidates_sql, parameters={"primary_record_id": primary_record_id})
 
-    scores = compare_records(  # noqa: F841
+    scores_df = compare_records(  # noqa: F841
         source_name,
         candidates_name,
         settings=MODEL_PATH,
         db_api=db_api,
         sql_cache_key="score_records_sql",
-    ).as_duckdbpyrelation()
+    )
+
+    sql = f"CREATE TABLE twins AS {filter_twins_sql(scores_df.physical_name)}"
+    connection_duckdb.execute(sql)
 
     end_time = time.perf_counter()
     logger.info("Time taken: %.2f seconds", end_time - start_time)
 
     if return_scores_only:
-        return connection_duckdb.sql("SELECT match_id_l, match_id_r, match_probability, match_weight FROM scores")
+        return connection_duckdb.sql(
+            "SELECT match_id_l, match_id_r, match_probability, match_weight, possible_twins FROM twins",
+        )
     else:
-        return connection_duckdb.sql("SELECT * FROM scores")
+        return connection_duckdb.sql("SELECT * FROM twins")
