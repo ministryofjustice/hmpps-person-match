bf9d0ae15f6c9d944406e19dd711128967e62fb0
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-10-16 09:33:00 +0100

    CPR-767 change info endpoint to match kotlin and typescript projects (#445)
    * CPR-767 change info endpoint to match kotlin and typescript projects
    
    * CPR-767 fix duplicate test

--- hmpps_person_match/models/info.py
+++ hmpps_person_match/models/info.py
@@ -1,11 +1,29 @@
 from pydantic import BaseModel
 
 
+class Build(BaseModel):
+    """
+    Part of Info response model
+    """
+    version: str
+
+class Commit(BaseModel):
+    """
+    Part of Info response model
+    """
+    id: str
+
+class Git(BaseModel):
+    """
+    Part of Info response model
+    """
+    branch: str
+    commit: Commit
+
 class Info(BaseModel):
     """
     Info response model
     """
+    build: Build
+    git: Git
 
-    version: str
-    commit_id: str
-    branch: str
