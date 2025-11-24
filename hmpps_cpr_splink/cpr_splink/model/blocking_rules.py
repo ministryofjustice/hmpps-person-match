f4a9e224f63c7e48b7a83364a61a63861782ca3c
Robin Linacre <robinlinacre@hotmail.com>
2025-11-24 15:24:19 +0000

    slim down to only a postcode GIN index and blocking rules

--- hmpps_cpr_splink/cpr_splink/model/blocking_rules.py
+++ hmpps_cpr_splink/cpr_splink/model/blocking_rules.py
@@ -17,11 +17,7 @@ blocking_rules_for_prediction_tight_for_candidate_search = [
         "substr(last_name_std, 1, 2)",
     ),
     And(block_on("forename_first", "last_name_first"), POSTCODE_INTERSECT),
-    # block_on("forename_first", "last_name_first", "postcode_first"),  # noqa: ERA001
-    # block_on("date_of_birth", "postcode_last"),  # noqa: ERA001
-    # CustomRule("l.date_of_birth = r.date_of_birth and l.postcode_first = r.postcode_second"),  # noqa: ERA001
-    And(block_on("date_of_birth"), SENTENCE_DATE_INTERSECT),
-    # block_on("sentence_date_first", "date_of_birth"),  # noqa: ERA001
+    block_on("sentence_date_first", "date_of_birth"),
     block_on("forename_last", "last_name_last", "date_of_birth"),
     block_on("forename_first", "last_name_last", "date_of_birth"),
     block_on("first_and_last_name_std", "name_2_std"),
@@ -30,21 +26,14 @@ blocking_rules_for_prediction_tight_for_candidate_search = [
         block_on("substr(name_1_std, 1, 2)", "substr(last_name_std, 1, 2)"),
         POSTCODE_INTERSECT,
     ),
-    # block_on("substr(name_1_std, 1, 2)", "substr(last_name_std, 1, 2)", "postcode_first"),  # noqa: ERA001
-    # block_on("substr(name_1_std, 1, 2)", "substr(last_name_std, 1, 2)", "postcode_last"),  # noqa: ERA001
-    And(
-        block_on("substr(name_1_std, 1, 2)", "substr(last_name_std, 1, 2)"),
-        SENTENCE_DATE_INTERSECT,
+    block_on(
+        "substr(name_1_std, 1, 2)",
+        "substr(last_name_std, 1, 2)",
+        "sentence_date_last",
     ),
     CustomRule(
         "l.name_1_std = r.last_name_std and l.last_name_std = r.name_1_std and l.date_of_birth = r.date_of_birth",
     ),
     block_on("override_marker"),
     block_on("master_defendant_id"),
-    # CustomRule(
-    #     "l.postcode_arr && r.postcode_arr",
-    # ),
-    # CustomRule(
-    #     "l.date_of_birth_arr && r.date_of_birth_arr",
-    # ),
 ]
--- hmpps_person_match/db/migrations/versions/2857e412d879_add_gin_index_for_arrays.py
+++ hmpps_person_match/db/migrations/versions/2857e412d879_add_gin_index_for_arrays.py
@@ -25,23 +25,7 @@ def upgrade() -> None:
         schema="personmatch",
         postgresql_using="gin",
     )
-    op.create_index(
-        "idx_date_of_birth_gin",
-        columns=["date_of_birth_arr"],
-        table_name="person",
-        schema="personmatch",
-        postgresql_using="gin",
-    )
-    op.create_index(
-        "idx_sentence_date_arr_gin",
-        columns=["sentence_date_arr"],
-        table_name="person",
-        schema="personmatch",
-        postgresql_using="gin",
-    )
 
 
 def downgrade() -> None:
     op.drop_index("idx_postcode_arr_gin", table_name="person")
-    op.drop_index("idx_date_of_birth_gin", table_name="person")
-    op.drop_index("idx_sentence_date_arr_gin", table_name="person")
