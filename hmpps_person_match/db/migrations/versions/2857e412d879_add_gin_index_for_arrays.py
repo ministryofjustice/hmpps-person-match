77262b0cd0cda958e4f828056f2fd3115b8e4a29
Robin Linacre <robinlinacre@hotmail.com>
2025-11-25 13:13:35 +0000

    indexing

--- hmpps_person_match/db/migrations/versions/2857e412d879_add_gin_index_for_arrays.py
+++ hmpps_person_match/db/migrations/versions/2857e412d879_add_gin_index_for_arrays.py
@@ -26,6 +26,92 @@ def upgrade() -> None:
         postgresql_using="gin",
     )
 
+    # Create indices to support the non-postcode parts of the new blocking rules
+    op.create_index(
+        "idx_date_of_birth",
+        columns=["date_of_birth"],
+        table_name="person",
+        schema="personmatch",
+    )
+    op.create_index(
+        "idx_forename_first__last_name_first",
+        columns=["forename_first", "last_name_first"],
+        table_name="person",
+        schema="personmatch",
+    )
+    op.execute(
+        """
+        create index idx_substring_name_1_std__substring_last_name_std
+        on personmatch.person (
+            SUBSTRING(name_1_std FROM 1 FOR 2),
+            SUBSTRING(last_name_std FROM 1 FOR 2)
+        );
+        """,
+    )
+
+    op.drop_index(
+        "idx_date_of_birth__postcode_first",
+        table_name="person",
+        schema="personmatch",
+    )
+    op.drop_index(
+        "idx_forename_first__last_name_first__postcode_first",
+        table_name="person",
+        schema="personmatch",
+    )
+    op.drop_index(
+        "idx_date_of_birth__postcode_last",
+        table_name="person",
+        schema="personmatch",
+    )
+    op.drop_index(
+        "idx_substring_name_1_std__substring_last_name_std__postcode_first",
+        table_name="person",
+        schema="personmatch",
+    )
+    op.drop_index(
+        "idx_substring_name_1_std__substring_last_name_std__postcode_last",
+        table_name="person",
+        schema="personmatch",
+    )
+
 
 def downgrade() -> None:
-    op.drop_index("idx_postcode_arr_gin", table_name="person")
+    op.execute(
+        "create index idx_date_of_birth__postcode_first on personmatch.person (date_of_birth, postcode_first);",
+    )
+    op.execute(
+        """
+        create index idx_forename_first__last_name_first__postcode_first
+        on personmatch.person (forename_first, last_name_first, postcode_first);
+        """,
+    )
+    op.execute(
+        "create index idx_date_of_birth__postcode_last on personmatch.person (date_of_birth, postcode_last);",
+    )
+    op.execute(
+        """
+        create index idx_substring_name_1_std__substring_last_name_std__postcode_first
+        on personmatch.person (
+            SUBSTRING(name_1_std FROM 1 FOR 2),
+            SUBSTRING(last_name_std FROM 1 FOR 2),
+            postcode_first
+        );
+        """,
+    )
+    op.execute(
+        """
+        create index idx_substring_name_1_std__substring_last_name_std__postcode_last
+        on personmatch.person (
+            SUBSTRING(name_1_std FROM 1 FOR 2),
+            SUBSTRING(last_name_std FROM 1 FOR 2),
+            postcode_last
+        );
+        """,
+    )
+
+    op.drop_index("idx_date_of_birth", table_name="person", schema="personmatch")
+    op.drop_index("idx_forename_first__last_name_first", table_name="person", schema="personmatch")
+    op.drop_index("idx_substring_name_1_std__substring_last_name_std", table_name="person", schema="personmatch")
+
+    op.drop_index("idx_postcode_arr_gin", table_name="person", schema="personmatch")
