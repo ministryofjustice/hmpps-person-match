bbd12bf8dcc6a264f6b3e348c0d581185bbf1d28
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-12-10 09:20:33 +0000

    CPR-1010 get app to run locally and add correct example for is-cluster-valid (#523)

--- hmpps_person_match/models/cluster/is_cluster_valid.py
+++ hmpps_person_match/models/cluster/is_cluster_valid.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class IsClusterValid(BaseModel):
-    is_cluster_valid: bool = Field(alias="isClusterValid", examples=[True])
+    is_cluster_valid: bool = Field(alias="isClusterValid", examples=[False])
     clusters: list[list[str]] = Field(
         examples=[
-            ["27fb6f34-ead9-4c62-b1b0-327a0c05c526", "e73386ef-b7bd-485a-827d-0bc1e330d779"],
+            [["27fb6f34-ead9-4c62-b1b0-327a0c05c526"], ["e73386ef-b7bd-485a-827d-0bc1e330d779"]],
         ],
     )
 
